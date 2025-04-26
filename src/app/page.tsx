0d0a87394a9e9ec820a9837ea75cbc5fe8f42479
eric-waters <ericwaters2019@gmail.com>
2025-04-26 11:19:25 -0700

    home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import HeroSectionOne from "@/components/custom/hero-sections";
 import Navbar from "@/components/custom/navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
+      <div className="container mx-auto">
+        <Navbar />
+        <HeroSectionOne />
+      </div>
     </>
   );
 }
